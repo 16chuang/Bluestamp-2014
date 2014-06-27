f846f755f6387c519c01f5719121f3b212a20fd2
16chuang <16chuang@castilleja.org>
2014-06-26 22:05:18 -0700

    First commit

---
+++ Motor_Test/Motor_Test.ino
@@ -0,0 +1,70 @@
+// motor PWM 
+const int MOTOR_PWM = 9; 
+
+// change direction of motors
+// A = 1, B = 1 -- brake to Vcc
+// A = 1, B = 0 -- clockwise
+// A = 0, B = 1 -- counterclockwise
+// A = 0, B = 0 -- brake to GND
+const int MOTOR_IN_A = 10;
+const int MOTOR_IN_B = 11;
+
+/* ENCODERS 
+ * ----------------------------- */
+
+// encoder ports
+const int MOTOR_ENCODER_B = 12;
+const int MOTOR_ENCODER_A = 13;
+
+// encoder reading
+int encoderValue = 0;
+
+// read function prototype
+void readEncoder();
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Arduino pins to motor driver
+  pinMode(MOTOR_PWM, OUTPUT);
+  pinMode(MOTOR_IN_A, OUTPUT);
+  pinMode(MOTOR_IN_B, OUTPUT);
+  pinMode(MOTOR_ENCODER_A, INPUT);
+  pinMode(MOTOR_ENCODER_B, INPUT);
+}
+
+void loop() {
+  // PWM values: 25% = 64; 50% = 127; 75% = 191; 100% = 255
+  // control speed
+  analogWrite(MOTOR_PWM, 127);
+
+  // turn counter-clockwise
+  digitalWrite(MOTOR_IN_A, LOW);
+  digitalWrite(MOTOR_IN_B, HIGH);
+  
+  readEncoder();
+
+  delay(10);
+}
+
+int currentReadingA = LOW;
+int currentReadingB = LOW;
+int previousReadingA = LOW;
+
+void readEncoder() {
+  // get current encoder readings
+  currentReadingA = digitalRead(MOTOR_ENCODER_A);
+  currentReadingB = digitalRead(MOTOR_ENCODER_B);
+
+  if (previousReadingA == LOW && currentReadingA == HIGH) { // coming up on an edge
+    if (currentReadingB == HIGH) {
+    	encoderValue++; // clockwise
+    } else {
+    	encoderValue--; // counterclockwise
+    }
+  }
+
+  Serial.println(encoderValue);
+
+  previousReadingA = currentReadingA;
+}
---
+++ Motor_and_IMU/Motor_and_IMU.ino
@@ -0,0 +1,132 @@
+/* -----------------------------
+ * ----------- IMU ------------- 
+ * ----------------------------- */
+// libraries
+#include <Wire.h>
+#include <FreeSixIMU.h>
+#include <FIMU_ADXL345.h>
+#include <FIMU_ITG3200.h>
+
+FreeSixIMU IMU;
+const int AvgAngles = 3;
+
+/* UPDATE IMU ANGLE
+ ---------------------------- */
+float angles[5];
+float currAngle, prevAngle;
+float prevAngles[AvgAngles];
+int prevAnglesArrayIndex = 0;
+float anglesSum = 0;
+
+/* CALIBRATE IMU ANGLE
+ ---------------------------- */
+float calibrateSum = 0;
+float zeroAngle = 0;
+const int CALIBRATE_NUM_TIMES = 200;
+
+/* -----------------------------
+ * ---------- MOTOR ------------ 
+ * ----------------------------- */
+
+// motor PWM 
+const int MOTOR_PWM = 9; 
+
+// change direction of motors
+// A = 1, B = 0 -- clockwise
+// A = 0, B = 1 -- counterclockwise
+const int MOTOR_IN_A = 10;
+const int MOTOR_IN_B = 11;
+
+enum Direction {
+  FORWARD,
+  BACKWARD,
+};
+
+/* -----------------------------
+ * ------ PID CONSTANTS -------- 
+ * ----------------------------- */
+const float kP = 0.1;
+float setpoint;
+float command;
+
+/* -----------------------------
+ * --- FUNCTION PROTOTYPES ----- 
+ * ----------------------------- */
+void calibrateIMU();
+void updateAngle();
+float readIMU_Y();
+void moveMotors();
+void updateMotorsPID();
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Arduino pins to motor driver
+  pinMode(MOTOR_PWM, OUTPUT);
+  pinMode(MOTOR_IN_A, OUTPUT);
+  pinMode(MOTOR_IN_B, OUTPUT);
+  pinMode(MOTOR_ENCODER_A, INPUT);
+  pinMode(MOTOR_ENCODER_B, INPUT);
+  
+  
+  // IMU initialization
+  Wire.begin(); // IMU connection
+
+  IMU = FreeSixIMU();
+  delay(5);
+  IMU.init(); // Begin the IMU
+  delay(5);
+
+  calibrateIMU();
+
+  setpoint = zeroAngle;
+}
+
+void loop() {
+  updateAngle();
+  updateMotorsPID();
+  
+  delay(10);
+}
+
+void updateMotorsPID() {
+  command = kP * (currAngle - setpoint);
+  Direction direction = (command >= 0) ? FORWARD : BACKWARD;
+  int speed = max(abs(command), 255);
+  moveMotors(direction, speed);
+}
+
+void moveMotors(Direction direction, int speed) {
+  // PWM values: 25% = 64; 50% = 127; 75% = 191; 100% = 255
+  analogWrite(MOTOR_PWM, speed);
+
+  digitalWrite(MOTOR_IN_A, direction);
+  digitalWrite(MOTOR_IN_B, abs(1 - direction));
+}
+
+void calibrateIMU() {
+  for (int i = 0; i < CALIBRATE_NUM_TIMES; i++) {
+    calibrateSum += readIMU_Y();
+    Serial.println(readIMU_Y());
+  }
+  zeroAngle = calibrateSum / CALIBRATE_NUM_TIMES;
+
+  Serial.println("--------------- CALIBRATED ---------------");
+  Serial.println(zeroAngle);
+  Serial.println("--------------- CALIBRATED ---------------");
+}
+
+void updateAngle() {
+  prevAngles[prevAnglesArrayIndex] = readIMU_Y() - zeroAngle; // put calibrated pitch in prevAngles array
+  prevAnglesArrayIndex = (prevAnglesArrayIndex + 1) % AvgAngles; // increment prevAnglesArrayIndex index in prevAngles array
+
+  anglesSum = 0;
+  for (int i = 0; i < AvgAngles; i++) // average all angles in prevAngles array
+      anglesSum += prevAngles[i];
+  currAngle = anglesSum / AvgAngles;
+}
+
+float readIMU_Y() {
+  IMU.getYawPitchRoll(angles);
+  return angles[2];
+}
