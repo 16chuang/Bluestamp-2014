ad3b86447aed1aff0c8a3f00587c38593b19e70a
16chuang <16chuang@castilleja.org>
2014-07-17 12:19:16 -0700

    Added digital low pass filter to main code. Requires too much processing so robot starts to jitter again.

--- Motor_and_IMU/Motor_and_IMU.ino
+++ Motor_and_IMU/Motor_and_IMU.ino
@@ -62,18 +62,23 @@ const int L_ENCODER_B = 7;
 const int R_ENCODER_A = 3;
 const int R_ENCODER_B = 8;
 
-volatile long encoderRCount = 0;
-volatile long encoderLCount = 0;
-
+// dt calculation
 bool leftEncTimeState, rightEncTimeState = true;
 long leftEnc_t1, rightEnc_t1 = 0;
 long leftEnc_t2, rightEnc_t2 = 0;
 long leftEnc_dt, rightEnc_dt = 0;
 
+// encoder direction (-1 or 1 coefficient)
 int leftEncDirection, rightEncDirection = ENC_FORWARD;
 
+// speed calculation constants
 const int THRESHOLD_DT = 50; // number of milliseconds of not moving for speed to count as 0
 const float MILLIS_TO_SEC = 1000;
+
+// digital low pass filter stuff (digitalSmooth function)
+const int NUM_FILTER_SAMPLES = 13; // must be odd
+int rightSpeedSmoothArray[NUM_FILTER_SAMPLES];   // array for holding raw left speed
+int leftSpeedSmoothArray[NUM_FILTER_SAMPLES];   // array for holding raw right speed
 
 /* -----------------------------
  * ---- SPEED TO ANGLE PID -----
@@ -162,7 +167,7 @@ void loop() {
   pitch = COMPLEMENTARY_GAIN * (lastPitch + getGyroYRate() * loopTime / 1000) + (1 - COMPLEMENTARY_GAIN) * (getAccY() - zeroIMUAngle);
   lastPitch = pitch;
 
-//  Serial.println(pitch);
+  //  Serial.println(pitch);
 
   if (timeChange >= dt) {
     // use pitch and PID controller to calculate motor motorCommand
@@ -178,10 +183,10 @@ void loop() {
  === SPEED TO ANGLE PID + GET SPEED ===
  ====================================== */
 void speedToAnglePID() {
-  Serial.print(getLeftSpeed()); Serial.print('\t'); Serial.println(getRightSpeed());
+  Serial.println(digitalSmooth((int)getRawRightSpeed(), rightSpeedSmoothArray));
 }
 
-float getLeftSpeed() {
+float getRawLeftSpeed() {
   if ((millis() - leftEnc_t2) < THRESHOLD_DT) {
     return leftEncDirection * (MILLIS_TO_SEC / leftEnc_dt);
   } else {
@@ -189,7 +194,7 @@ float getLeftSpeed() {
   }
 }
 
-float getRightSpeed() {
+float getRawRightSpeed() {
   if ((millis() - rightEnc_t2) < THRESHOLD_DT) {
     return rightEncDirection * (MILLIS_TO_SEC / rightEnc_dt);
   } else {
@@ -267,7 +272,7 @@ void leftEncoder() {
   } else {
     leftEncDirection = ENC_BACKWARD;
   }
-  
+
   // record dt time difference in milliseconds
   if (leftEncTimeState) {
     leftEnc_t1 = millis();
@@ -286,7 +291,7 @@ void rightEncoder() {
   } else {
     rightEncDirection = ENC_BACKWARD;
   }
-  
+
   // record dt time difference in milliseconds
   if (rightEncTimeState) {
     rightEnc_t1 = millis();
@@ -296,6 +301,51 @@ void rightEncoder() {
     rightEnc_dt = rightEnc_t1 - rightEnc_t2;
     rightEncTimeState = true;
   }
+}
+
+/* ====================================
+ === ENCODER SPEED LOW PASS FILTER ====
+ ====================================== */
+// function from http://playground.arduino.cc/Main/DigitalSmooth
+// written by Paul Badger
+int digitalSmooth(int rawIn, int *sensSmoothArray) {    // "int *sensSmoothArray" passes an array to the function - the asterisk indicates the array name is a pointer
+  int j, k, temp, top, bottom;
+  long total;
+  static int i;
+  static int sorted[NUM_FILTER_SAMPLES];
+  boolean done;
+
+  i = (i + 1) % NUM_FILTER_SAMPLES;    // increment counter and roll over if necc. -  % (modulo operator) rolls over variable
+  sensSmoothArray[i] = rawIn;                 // input new data into the oldest slot
+
+  for (j = 0; j < NUM_FILTER_SAMPLES; j++) { // transfer data array into anther array for sorting and averaging
+    sorted[j] = sensSmoothArray[j];
+  }
+
+  done = 0;                // flag to know when we're done sorting
+  while (done != 1) {      // simple swap sort, sorts numbers from lowest to highest
+    done = 1;
+    for (j = 0; j < (NUM_FILTER_SAMPLES - 1); j++) {
+      if (sorted[j] > sorted[j + 1]) {    // numbers are out of order - swap
+        temp = sorted[j + 1];
+        sorted [j + 1] =  sorted[j] ;
+        sorted [j] = temp;
+        done = 0;
+      }
+    }
+  }
+  
+  // throw out top and bottom 15% of samples - limit to throw out at least one from top and bottom
+  bottom = max(((NUM_FILTER_SAMPLES * 15)  / 100), 1);
+  top = min((((NUM_FILTER_SAMPLES * 85) / 100) + 1  ), (NUM_FILTER_SAMPLES - 1));   // the + 1 is to make up for asymmetry caused by integer rounding
+  k = 0;
+  total = 0;
+  for ( j = bottom; j < top; j++) {
+    total += sorted[j];  // total remaining indices
+    k++;
+  }
+  
+  return total / k;    // divide by number of samples
 }
 
 /* ====================================
