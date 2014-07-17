d0d5c2f06372a499bff6e5e7b4ac88cfe31473da
16chuang <16chuang@castilleja.org>
2014-07-17 12:11:35 -0700

    Added low pass filter for speed to encoder test sketch.

--- Encoder_Test/Encoder_Test.ino
+++ Encoder_Test/Encoder_Test.ino
@@ -19,6 +19,13 @@ int leftEncDirection, rightEncDirection = ENC_FORWARD;
 const int THRESHOLD_DT = 50; // number of milliseconds of not moving for speed to count as 0
 const float MILLIS_TO_SEC = 1000;
 
+// digital low pass filter (digitalSmooth function)
+const int NUM_FILTER_SAMPLES = 13; // must be odd
+int rightSpeedSmoothArray[NUM_FILTER_SAMPLES];   // array for holding raw left speed
+int leftSpeedSmoothArray[NUM_FILTER_SAMPLES];   // array for holding raw right speed
+
+int smoothedRightSpeed, smoothedLeftSpeed = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(L_ENCODER_A, INPUT);
@@ -31,10 +38,11 @@ void setup() {
 }
 
 void loop() {
-  Serial.print(getLeftSpeed()); Serial.print('\t'); Serial.println(getRightSpeed());
+//  Serial.println(getRightSpeed());
+  Serial.println(digitalSmooth((int)getRawRightSpeed(), rightSpeedSmoothArray));
 }
 
-float getLeftSpeed() {
+float getRawLeftSpeed() {
   if ((millis() - leftEnc_t2) < THRESHOLD_DT) {
     return leftEncDirection * (MILLIS_TO_SEC / leftEnc_dt);
   } else {
@@ -42,7 +50,7 @@ float getLeftSpeed() {
   }
 }
 
-float getRightSpeed() {
+float getRawRightSpeed() {
   if ((millis() - rightEnc_t2) < THRESHOLD_DT) {
     return rightEncDirection * (MILLIS_TO_SEC / rightEnc_dt);
   } else {
@@ -51,14 +59,14 @@ float getRightSpeed() {
 }
 
 void leftEncoder() {
+  // determine direction
   if (digitalRead(L_ENCODER_B) == LOW) {
-    encoderLCount++;
     leftEncDirection = ENC_FORWARD;
   } else {
-    encoderLCount--;
     leftEncDirection = ENC_BACKWARD;
   }
   
+  // record dt time difference in milliseconds
   if (leftEncTimeState) {
     leftEnc_t1 = millis();
     leftEncTimeState = false;
@@ -70,14 +78,14 @@ void leftEncoder() {
 }
 
 void rightEncoder() {
+  // determine direction
   if (digitalRead(R_ENCODER_B) == LOW) {
-    encoderRCount--;
     rightEncDirection = ENC_FORWARD;
   } else {
-    encoderRCount++;
     rightEncDirection = ENC_BACKWARD;
   }
   
+  // record dt time difference in milliseconds
   if (rightEncTimeState) {
     rightEnc_t1 = millis();
     rightEncTimeState = false;
@@ -86,4 +94,63 @@ void rightEncoder() {
     rightEnc_dt = rightEnc_t1 - rightEnc_t2;
     rightEncTimeState = true;
   }
+}
+
+
+// function from http://playground.arduino.cc/Main/DigitalSmooth
+// written by Paul Badger
+int digitalSmooth(int rawIn, int *sensSmoothArray) {    // "int *sensSmoothArray" passes an array to the function - the asterisk indicates the array name is a pointer
+  int j, k, temp, top, bottom;
+  long total;
+  static int i;
+  // static int raw[NUM_FILTER_SAMPLES];
+  static int sorted[NUM_FILTER_SAMPLES];
+  boolean done;
+
+  i = (i + 1) % NUM_FILTER_SAMPLES;    // increment counter and roll over if necc. -  % (modulo operator) rolls over variable
+  sensSmoothArray[i] = rawIn;                 // input new data into the oldest slot
+
+  // Serial.print("raw = ");
+
+  for (j = 0; j < NUM_FILTER_SAMPLES; j++) { // transfer data array into anther array for sorting and averaging
+    sorted[j] = sensSmoothArray[j];
+  }
+
+  done = 0;                // flag to know when we're done sorting
+  while (done != 1) {      // simple swap sort, sorts numbers from lowest to highest
+    done = 1;
+    for (j = 0; j < (NUM_FILTER_SAMPLES - 1); j++) {
+      if (sorted[j] > sorted[j + 1]) {    // numbers are out of order - swap
+        temp = sorted[j + 1];
+        sorted [j + 1] =  sorted[j] ;
+        sorted [j] = temp;
+        done = 0;
+      }
+    }
+  }
+
+  /*
+    for (j = 0; j < (filterSamples); j++){    // print the array to debug
+      Serial.print(sorted[j]);
+      Serial.print("   ");
+    }
+    Serial.println();
+  */
+
+  // throw out top and bottom 15% of samples - limit to throw out at least one from top and bottom
+  bottom = max(((NUM_FILTER_SAMPLES * 15)  / 100), 1);
+  top = min((((NUM_FILTER_SAMPLES * 85) / 100) + 1  ), (NUM_FILTER_SAMPLES - 1));   // the + 1 is to make up for asymmetry caused by integer rounding
+  k = 0;
+  total = 0;
+  for ( j = bottom; j < top; j++) {
+    total += sorted[j];  // total remaining indices
+    k++;
+    // Serial.print(sorted[j]);
+    // Serial.print("   ");
+  }
+
+  //  Serial.println();
+  //  Serial.print("average = ");
+  //  Serial.println(total/k);
+  return total / k;    // divide by number of samples
 }
